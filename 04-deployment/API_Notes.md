b7c9a2083f860e13ace894da364e79355942304e
tajpatel58 <tajpatel58@gmail.com>
2023-12-06 00:11:23 +0000

    adding more examples on pydantic

--- 04-deployment/API_Notes.md
+++ 04-deployment/API_Notes.md
@@ -69,22 +69,3 @@
 }'`
 
 - Note we can also use requests to exchange data with a web server. 
-
-### PyDantic:
-
-- Here we take a small detour on pydantic, which is a package used to enforce structure of inputs and outputs in our API methods. 
-- So why might this be useful? 
-  - Well suppose we get a "PUT" method to update the entry into a database. Database have schemas so we can't simply add any data into a particular table. Hence we pydantic to ensure that the correct keys are present. 
-  - It provides input and output valdation of methods. Eg if a POST method should return an int, and we return a float, then that's a problem. 
-- A bit like the base class of Pandas is: "DataFrame", pydnatic has "BaseModel", so the most "common" object we would be using in pydantic is a pydantic model.
-- We can think of a pydantic model as a dictionary where we can access the elements like: `person.name`, NOT square brackets. 
-- When creating an instance, we need to pass in values like keyword arguments to a function, 
-- Pydantic is clever and may try to case a string to an int, if the type is int for a variable. 
-
-- The following can be used to create a dictionary like object with a set structure:
-  - `from pydantic import BaseModel`
-  - `class Name_Age(BaseModel):`
-    -   `name : str`
-    -   `age : int`
-
-- In summary, type hints with Pydantic are essential when building API's.
--- 04-deployment/test_pydantic.py
+++ 04-deployment/test_pydantic.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Field
+from typing import Literal
+from pydantic import BaseModel, Field, validator
 from pydantic.types import constr
 
 class Person(BaseModel):
@@ -11,12 +12,81 @@ class ContactEntry(BaseModel):
     person : Person
     phone_number: constr(regex="[0-9]{11}", max_length=11)
 
+#Literal forces values to be out of a list.
+class Account(BaseModel):
+    account_id : str
+    employee_size : Literal["small", "medium", "large"]
+
+    # validator is run each time instance is instantiated, saves us a call.
+    @validator("account_id")
+    def account_id_validator(cls, value):
+        # want account_id to be 9 digits and each number only appears once:
+        list_of_digits = list(value)
+        num_digits = len(list_of_digits)
+        if num_digits != 9:
+            raise ValueError(f"Account Id needs to be 9 characters not: {num_digits}")
+        
+        digits_seen = []
+        for dig in list_of_digits:
+            if dig in digits_seen:
+                raise ValueError(f"Repeated digits not allowed: account id has digit: {dig} repeated.")
+            else:
+                digits_seen.append(dig)
+        print(f"Account Id: {value} is acceptable")
+        
+        # MUST RETURN THE VALUE IF ACCEPTABLE:
+        return value
+
 
 p1 = Person(name="Taj", age="24")
-
 p2 = Person(age=10, name="Taj")
+phone_error = "1231"
 phone_2= "01982711112"
 
-contact_entry = ContactEntry(person=p2, phone_number=phone_2)
-print(contact_entry.phone_number)
-print(contact_entry.person.name)
+# Following should run smoothly as phone number is valid.
+try:
+    contact_entry = ContactEntry(person=p2, phone_number=phone_2)
+    print(f"Contact Entry Created Succesfully:")
+    print(contact_entry.dict())
+except Exception as e:
+    print(e)
+
+# Following should fail as phone number is valid.
+try:
+    contact_entry = ContactEntry(person=p2, phone_number=phone_error)
+    print(f"Contact Entry Created Succesfully:")
+    print(contact_entry.dict())
+except Exception as e:
+    print(e)
+
+# Following should throw an error as digit 7 repeated in account_id
+try:
+    account_1 = Account(account_id="123456779", employee_size="small")
+    print(f"Account Created:")
+    print(account_1.dict())
+except Exception as e:
+    print(e)
+
+# Following should throw an error as length of account id only 3 digits.
+try:
+    account_1 = Account(account_id="123", employee_size="small")
+    print(f"Account Created:")
+    print(account_1.dict())
+except Exception as e:
+    print(e)
+
+# Following should throw an error as employee size is extra large which is not in list.
+try:
+    account_1 = Account(account_id="123456789", employee_size="extra_large")    
+    print(f"Account Created:")
+    print(account_1.dict())
+except Exception as e:
+    print(e)
+
+# Following should throw an error as employee size is extra large which is not in list.
+try:
+    account_1 = Account(account_id="123456789", employee_size="small")
+    print(f"Account Created:")
+    print(account_1.dict())
+except Exception as e:
+    print(e)
